be32bd9997ee8c8297e4b6d1919ae47fefa188f9
Pedro Andrade <pedrofreitasdeandrade@gmail.com>
2022-07-28 19:04:17 -0300

    implementar js do scroll

---
+++ js/configScroll.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal({
+    distance: '50px',
+    duration: 800,
+    origin: 'top',
+    reset: false
+})
+
+
+// Delay
+
+sr.reveal('.carousel', { delay: 250 })
